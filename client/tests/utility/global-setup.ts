7e9ea62fa7ede0e129b397ab58657d03941ad025
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-06-23 15:17:44 +0200

    feat: add global setup script for Playwright end-to-end testing

---
+++ client/tests/utility/global-setup.ts
@@ -0,0 +1,21 @@
+// global-setup.ts
+import { chromium } from "@playwright/test";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
+
+export default async function globalSetup() {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto("http://localhost:3000/auth/login");
+  await page
+    .getByRole("textbox", { name: /email|username/i })
+    .fill(process.env.USER_EMAIL!);
+  await page
+    .getByRole("textbox", { name: /password/i })
+    .fill(process.env.USER_PASSWORD!);
+  await page.getByRole("button", { name: "Continue", exact: true }).click();
+  await page.waitForURL("http://localhost:3000/");
+  // Save storage state
+  await page.context().storageState({ path: "storageState.json" });
+  await browser.close();
+}
