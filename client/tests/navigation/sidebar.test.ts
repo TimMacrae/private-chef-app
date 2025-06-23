c986b714472858374ef55e08f0bf55588bd56bf0
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-06-23 15:17:54 +0200

    feat: add sidebar navigation tests using Playwright

---
+++ client/tests/navigation/sidebar.test.ts
@@ -0,0 +1,25 @@
+import { sidebarMenu } from "@/components/navigation/sidebarMenu.config";
+import test, { expect } from "@playwright/test";
+
+test.describe("navigation", () => {
+  test("should display the sidebar bar with the correct items", async ({
+    page,
+  }) => {
+    await page.goto("/");
+    await expect(page.getByTestId("sidebar")).toBeVisible();
+    sidebarMenu.forEach(async (item) => {
+      await expect(page.getByTestId(item.dataTestId)).toBeVisible();
+    });
+  });
+
+  test("mobile sidebar", async ({ page }) => {
+    await page.setViewportSize({ width: 375, height: 812 }); // iPhone X
+    await page.goto("/");
+    await expect(page.getByTestId("sidebar")).not.toBeVisible();
+    await expect(page.getByTestId("sidebar-trigger-mobile")).toBeVisible();
+    await page.getByTestId("sidebar-trigger-mobile").click();
+    sidebarMenu.forEach(async (item) => {
+      await expect(page.getByTestId(item.dataTestId)).toBeVisible();
+    });
+  });
+});
