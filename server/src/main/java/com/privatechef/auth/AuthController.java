63517e20325a4c8985be40747eef3aa86a2b0a38
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-07-02 15:16:00 +0200

    refactor AuthController to use AuthService for user info retrieval

--- server/src/main/java/com/privatechef/auth/AuthController.java
+++ server/src/main/java/com/privatechef/auth/AuthController.java
@@ -1,6 +1,6 @@
 package com.privatechef.auth;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -8,41 +8,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.Map;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-    @Value("${api.roles.namespace}")
-    private String ROLES_NAMESPACE;
+    private final AuthService authService;
 
     @GetMapping("/me")
     public ResponseEntity<Map<String, Object>> getCurrentUser(@AuthenticationPrincipal Jwt jwt) {
-        System.out.println("ME: " + jwt.getClaims());
-        Map<String, Object> userInfo = new HashMap<>();
 
-        if (jwt.getClaim("sub") != null) {
-            userInfo.put("id", jwt.getClaim("sub"));
-        }
-
+        Map<String, Object> userInfo = authService.getCurrentUser(jwt);
         return ResponseEntity.ok(userInfo);
     }
 
     @GetMapping("/admin")
     public ResponseEntity<Map<String, Object>> getCurrentAdminUser(@AuthenticationPrincipal Jwt jwt) {
-        System.out.println("ME: " + jwt.getClaims());
-        Map<String, Object> userInfo = new HashMap<>();
 
-        if (jwt.getClaim("sub") != null) {
-            userInfo.put("id", jwt.getClaim("sub"));
-        }
-
-        if (jwt.getClaim(ROLES_NAMESPACE) != null) {
-            userInfo.put("roles", jwt.getClaim(ROLES_NAMESPACE));
-        }
-
-        return ResponseEntity.ok(userInfo);
+        Map<String, Object> userAdminInfo = authService.getCurrentAdminUser(jwt);
+        return ResponseEntity.ok(userAdminInfo);
     }
 
 }
