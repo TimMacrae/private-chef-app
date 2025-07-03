0501dcf4fb9bfc7faf845c4e7bcee62c39c7fc65
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-07-03 08:33:35 +0200

    update global setup to use E2E environment variables for login credentials

--- client/tests/utility/global-setup.ts
+++ client/tests/utility/global-setup.ts
@@ -9,10 +9,10 @@ export default async function globalSetup() {
   await page.goto("http://localhost:3000/auth/login");
   await page
     .getByRole("textbox", { name: /email|username/i })
-    .fill(process.env.USER_EMAIL!);
+    .fill(process.env.E2E_USER_EMAIL!);
   await page
     .getByRole("textbox", { name: /password/i })
-    .fill(process.env.USER_PASSWORD!);
+    .fill(process.env.E2E_USER_PASSWORD!);
   await page.getByRole("button", { name: "Continue", exact: true }).click();
   await page.waitForURL("http://localhost:3000/");
   // Save storage state
