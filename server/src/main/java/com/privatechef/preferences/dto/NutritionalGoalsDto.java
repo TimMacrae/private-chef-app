ef501d63b144ad6f2490d957e4cac6ca93260acc
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-07-21 16:17:23 +0200

    feat: add NutritionalGoalsDto for user preferences

---
+++ server/src/main/java/com/privatechef/preferences/dto/NutritionalGoalsDto.java
@@ -0,0 +1,11 @@
+package com.privatechef.preferences.dto;
+
+import lombok.Data;
+
+@Data
+public class NutritionalGoalsDto {
+    private Integer dailyCalories;
+    private Integer proteinGrams;
+    private Integer carbGrams;
+    private Integer fatGrams;
+}
