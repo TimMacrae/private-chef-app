619162bc1dbd0ca3fa9b02828d23412c6a32c3c1
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-08-28 11:03:03 +0200

    add Autowired

--- server/src/test/java/com/privatechef/auth/AuthControllerTest.java
+++ server/src/test/java/com/privatechef/auth/AuthControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,6 +23,9 @@ public class AuthControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    JwtDecoder jwtDecoder;
 
     @Test
     void getCurrentUser_shouldReturnUnauthorizedWithoutToken() throws Exception {
