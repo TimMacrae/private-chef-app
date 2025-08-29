5986e5f646a83af5edf96d8bef6df029872ce111
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-08-29 13:45:17 +0200

    update tests for data structure boolean

--- server/src/test/java/com/privatechef/preferences/dto/SeasonalPreferencesDtoTest.java
+++ server/src/test/java/com/privatechef/preferences/dto/SeasonalPreferencesDtoTest.java
@@ -2,8 +2,6 @@ package com.privatechef.preferences.dto;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,18 +10,18 @@ class SeasonalPreferencesDtoTest {
     @Test
     void preferencesDto_ShouldBeCreatedCorrectly() {
         SeasonalPreferencesDto dto = new SeasonalPreferencesDto();
-        List<String> summer = new ArrayList<>(List.of("Watermelon"));
-        List<String> winter = new ArrayList<>(List.of("Soups"));
-        List<String> spring = new ArrayList<>(List.of("Salat"));
-        List<String> autumn = new ArrayList<>(List.of("Wild"));
+        boolean summer = false;
+        boolean winter = false;
+        boolean spring = true;
+        boolean autumn = false;
         dto.setSummer(summer);
         dto.setAutumn(autumn);
         dto.setWinter(winter);
         dto.setSpring(spring);
 
-        assertEquals(summer, dto.getSummer());
-        assertEquals(autumn, dto.getAutumn());
-        assertEquals(winter, dto.getWinter());
-        assertEquals(spring, dto.getSpring());
+        assertEquals(summer, dto.isSummer());
+        assertEquals(autumn, dto.isAutumn());
+        assertEquals(winter, dto.isWinter());
+        assertEquals(spring, dto.isSpring());
     }
 }
