1b5c5b9e050afed455dd244ee8f13b6e3781cbe0
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-08-29 13:45:53 +0200

    update SeasonalPreferencesDto for data structure boolean

--- server/src/main/java/com/privatechef/preferences/dto/SeasonalPreferencesDto.java
+++ server/src/main/java/com/privatechef/preferences/dto/SeasonalPreferencesDto.java
@@ -1,15 +1,11 @@
 package com.privatechef.preferences.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 public class SeasonalPreferencesDto {
-    private List<@NotBlank String> spring = new ArrayList<>();
-    private List<@NotBlank String> summer = new ArrayList<>();
-    private List<@NotBlank String> autumn = new ArrayList<>();
-    private List<@NotBlank String> winter = new ArrayList<>();
+    private boolean spring = false;
+    private boolean summer = false;
+    private boolean autumn = false;
+    private boolean winter = false;
 }
