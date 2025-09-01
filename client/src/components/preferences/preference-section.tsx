aaee2514c8247870d0a21ca4fa1f62927825668f
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-09-01 16:18:02 +0200

    add test id and tests

--- client/src/components/preferences/preference-section.tsx
+++ client/src/components/preferences/preference-section.tsx
@@ -10,7 +10,12 @@ export const PreferenceSection = ({
 }: PreferenceSectionProps) => {
   return (
     <div className="mt-4" data-testid={dataTestId}>
-      <h2 className="text-lg font-semibold mb-4">{title}</h2>
+      <h2
+        className="text-lg font-semibold mb-4"
+        data-testid={`${dataTestId}-title`}
+      >
+        {title}
+      </h2>
       <div className="space-y-3">{children}</div>
     </div>
   );
