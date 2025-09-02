e7c3533b1a15a5501efe47bf876ad298f9535d15
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-09-02 16:11:33 +0200

    add tests and test ids for preferences-item-list

--- client/src/components/preferences/preferences-inputs/preferences-item-list.tsx
+++ client/src/components/preferences/preferences-inputs/preferences-item-list.tsx
@@ -14,7 +14,12 @@ export function PreferenceItemList({
   return (
     <div className="mb-4" data-testid={`preference-item-list-${preferenceKey}`}>
       <div className="flex mb-2">
-        <h3 className="flex items-center font-medium text-md">{title}</h3>
+        <h3
+          className="flex items-center font-medium text-md"
+          data-testid={`preference-item-title-${preferenceKey}`}
+        >
+          {title}
+        </h3>
         <PreferencesItemListDialog
           title={title}
           preferenceKey={preferenceKey}
