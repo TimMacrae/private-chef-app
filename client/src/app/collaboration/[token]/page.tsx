d863292a090cd40c9cf3b261a428dfae18369281
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-09-16 08:32:19 +0200

    add token page

---
+++ client/src/app/collaboration/[token]/page.tsx
@@ -0,0 +1,23 @@
+import { apiConfig } from "@/lib/api/api-config";
+import { auth0 } from "@/lib/auth/auth0";
+
+import { redirect } from "next/navigation";
+import { addCollaborationIdToCollaborationUserByTokenServerAction } from "@/app/actions/collaboration-server.actions";
+
+export default async function CollaborationInvitationPage({
+  params,
+}: {
+  params: { token: string };
+}) {
+  const session = await auth0.getSession();
+
+  if (!session) {
+    redirect(apiConfig.URL.AUTH_REGISTER);
+  }
+
+  const param = await params;
+  const token = param.token;
+  await addCollaborationIdToCollaborationUserByTokenServerAction({ token });
+
+  redirect(apiConfig.URL.COLLABORATION);
+}
