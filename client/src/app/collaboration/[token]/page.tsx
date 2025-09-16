8bb37689a332a6f074274e69a1cde46b28e8b631
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-09-16 13:06:16 +0200

    fix build error await params

--- client/src/app/collaboration/[token]/page.tsx
+++ client/src/app/collaboration/[token]/page.tsx
@@ -4,18 +4,17 @@ import { auth0 } from "@/lib/auth/auth0";
 import { redirect } from "next/navigation";
 import { addCollaborationIdToCollaborationUserByTokenServerAction } from "@/app/actions/collaboration-server.actions";
 
-export default async function CollaborationInvitationPage({
-  params,
-}: {
-  params: { token: string };
-}) {
+type Props = { params: Promise<{ token: string }> };
+
+export default async function CollaborationInvitationPage({ params }: Props) {
+  const { token } = await params;
+
   const session = await auth0.getSession();
 
   if (!session) {
     redirect(apiConfig.URL.AUTH_REGISTER);
   }
 
-  const { token } = params;
   await addCollaborationIdToCollaborationUserByTokenServerAction({ token });
 
   redirect(apiConfig.URL.COLLABORATION);
