ca5b0136be53f8c7e635743226abfc3531f110e4
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-09-17 15:46:54 +0200

    update endpoints

--- server/src/main/java/com/privatechef/config/EndpointsConfig.java
+++ server/src/main/java/com/privatechef/config/EndpointsConfig.java
@@ -22,4 +22,7 @@ public final class EndpointsConfig {
 
     // COLLABORATION
     public static final String COLLABORATION = API_BASE + "/collaboration";
+
+    // RECIPES
+    public static final String RECIPES = API_BASE + "/recipes";
 }
