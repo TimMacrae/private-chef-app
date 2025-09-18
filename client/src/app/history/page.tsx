24eaff09e725cc0e39a7062fce5ffb27c3e0ebad
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-09-18 15:52:01 +0200

    add history page

---
+++ client/src/app/history/page.tsx
@@ -0,0 +1,36 @@
+import { getRecipesAction } from "@/app/actions/recipes-server.actions";
+import { History } from "@/components/history/history";
+import { LayoutContentContainer } from "@/components/layout/layout-content-container";
+
+import { apiConfig } from "@/lib/api/api-config";
+import { auth0 } from "@/lib/auth/auth0";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { redirect } from "next/navigation";
+
+export default async function HistoryPage() {
+  const session = await auth0.getSession();
+
+  if (!session) {
+    redirect(apiConfig.URL.HOME);
+  }
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: [apiConfig.QUERY_KEYS.RECIPES],
+    queryFn: getRecipesAction,
+    staleTime: 1000 * 60 * 5, // 5 minutes
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <LayoutContentContainer>
+        <History />
+      </LayoutContentContainer>
+    </HydrationBoundary>
+  );
+}
