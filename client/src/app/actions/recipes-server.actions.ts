3ad4d1be77d0dec872c6d2bb07ad1c9eaada29c6
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-09-18 15:51:26 +0200

    add recipe action

--- client/src/app/actions/recipes-server.actions.ts
+++ client/src/app/actions/recipes-server.actions.ts
@@ -1,9 +1,21 @@
 "use server";
 
-import { Recipes, RecipesSchema } from "@/components/recipes/recipes.type";
+import {
+  Recipe,
+  Recipes,
+  RecipeSchema,
+  RecipesSchema,
+} from "@/components/recipes/recipes.type";
 import { apiConfig } from "@/lib/api/api-config";
 import { serverRequest } from "@/lib/api/api-server-request-handler";
 
 export async function getRecipesAction(): Promise<Recipes> {
   return await serverRequest(apiConfig.API.RECIPES, RecipesSchema);
+}
+
+export async function getRecipeAction(recipeId: string): Promise<Recipe> {
+  return await serverRequest(
+    `${apiConfig.API.RECIPES}/${recipeId}`,
+    RecipeSchema
+  );
 }
